52f8979d29cb1f330864b5fdc796c8db6fa6b067
Shurap <bap.essp@mail.ru>
2020-06-01 19:37:06 +0300

    finish deploy

---
+++ client/build/precache-manifest.0ac113ba1ca2b342107c50f28f5c076b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c86de7e42f1f9d7b059138a874fd6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c519fbc23508fd1ac2aa",
+    "url": "/static/css/main.713695d6.chunk.css"
+  },
+  {
+    "revision": "37319e8b46ef5a9d4f65",
+    "url": "/static/js/2.286ce6b0.chunk.js"
+  },
+  {
+    "revision": "e2a595cebc19315c6e6690cf04f42e5b",
+    "url": "/static/js/2.286ce6b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c519fbc23508fd1ac2aa",
+    "url": "/static/js/main.a2668c4c.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "b66994280b1dd09b13ecf3c91f92972c",
+    "url": "/static/media/htc-hand.b6699428.woff2"
+  },
+  {
+    "revision": "2084f6746350f268725b46a5a58ca102",
+    "url": "/static/media/paper.2084f674.jpg"
+  }
+]);
