e446685c5b58fd06b8f3177a012a616e07797feb
Shurap <bap.essp@mail.ru>
2020-06-02 12:47:52 +0300

    deploy 2.3

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ db.once('open', function () {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/public'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
   });
 }
 
